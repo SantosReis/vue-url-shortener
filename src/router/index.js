4f10e854dba17bb305518307cdba9d0dfb51f0fd
Marcus Reis <mvsantosreis@gmail.com>
2024-05-22 14:05:57 +0100

    SignIn Signup UI

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,28 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../views/Home.vue'
 import Profile from '../views/Profile.vue'
+import SigninView from '@/views/Authentication/SigninView.vue'
+import SignupView from '@/views/Authentication/SignupView.vue'
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/profile', component: Profile }
+  { path: '/profile', component: Profile },
+  {
+    path: '/signin',
+    name: 'signin',
+    component: SigninView,
+    meta: {
+      title: 'Signin'
+    }
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignupView,
+    meta: {
+      title: 'Signup'
+    }
+  }
 ]
 
 const router = createRouter({
