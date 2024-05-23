bd08cdc9431d09f31d5d05962185b36c7cb0ae1e
Marcus Reis <mvsantosreis@gmail.com>
2024-05-23 13:48:36 +0100

    Axios setup

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import router from './router'
 import Toast from 'vue-toastification'
+
 import 'vue-toastification/dist/index.css'
 // import "./assets/style.css";
 import './assets/css/satoshi.css'
 import './assets/css/style.css'
+
+import './axios'
 import App from './App.vue'
 
 const pinia = createPinia()
