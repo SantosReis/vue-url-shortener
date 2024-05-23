eafee9b0a91707c90ab157640a2b6364adfbc07d
Marcus Reis <mvsantosreis@gmail.com>
2024-05-23 13:49:01 +0100

    Auth Store and Route

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import Profile from '../views/Profile.vue'
 import SigninView from '@/views/Authentication/SigninView.vue'
 import SignupView from '@/views/Authentication/SignupView.vue'
 
+import { useAuthStore } from '@/stores/auth'
+
 const routes = [
   { path: '/', component: Home },
   { path: '/profile', component: Profile },
@@ -29,6 +31,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach(async (to) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/signin', '/signup']
+  const authRequired = !publicPages.includes(to.path)
+  const auth = useAuthStore()
+
+  if (authRequired && !auth.user) {
+    auth.returnUrl = to.fullPath
+    return '/signin'
+  }
 })
 
 export default router
